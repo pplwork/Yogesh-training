32309c92cdc877989bdb38aa59988f97012527ea
Yogesh902 <yogesh@pplwork.com>
2022-02-10 13:20:59 +0530

    React Hooks Started

---
+++ ReactJS/ReactHooks/counter/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Counter1 from "./Components/1. useState Hook/Counter1";
+import Counter2 from "./Components/1. useState Hook/Counter2";
+import Counter3 from "./Components/1. useState Hook/Counter3";
+import Counter4 from "./Components/1. useState Hook/Counter4";
+import ClassCounter1 from "./Components/2. useEffect Hook/ClassCounter1";
+import UseEffectCounter1 from "./Components/2. useEffect Hook/UseEffectCounter1";
+
+function App() {
+  return (
+    // 1.useState Hook...
+    // <Counter1/>
+    // <Counter2 />
+    // <Counter3 />
+    // <Counter4 />
+
+    // 2. useEffect hook
+    // <ClassCounter1 />
+    <UseEffectCounter1 />
+  );
+}
+
+export default App;
